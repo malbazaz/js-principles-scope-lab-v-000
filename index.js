bb94cbe68947690e9d3c85f12abc064cec479a87
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-21 14:30:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+var customerName;
+function upperCaseCustomerName(){
+  customerName.toUpperCase();
+}
+function setBestCustomer(){
+  var bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
